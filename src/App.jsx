4833ff0fe27a901f0ab13d74746596645cd2429d
vinthiyad <144670346+vinthiyad@users.noreply.github.com>
2023-12-08 22:27:33 +0530

    Created Pages And Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import {BrowserRouter ,Routes , Route}  from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
-    <div>
-
-<h1 className='bg-red-900'>test</h1>
-
-    </div>
+    <BrowserRouter >
+    <Routes>
+      <Route path="/" element={ < Home/>}/>
+      <Route path="/about" element={ < About/>}/>
+      <Route path="/profile" element={ < Profile/>}/>
+      <Route path="/signin" element={ < SignIn/>}/>
+      <Route path="/signup" element={ < SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
